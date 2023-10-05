a886dfedf36206d35275403f11682decfb615192
angusmellor <theangusmellor@gmail.com>
2023-10-06 00:43:04 +0100

    intial three commit

---
+++ src/routes/about/shader/vertex.glsl.js
@@ -0,0 +1,10 @@
+export default /* glsl */ `
+uniform float time
+varying vec2 vUv
+varying vec3 vPosition
+uniform vec2 pixels
+float PI = 3.141592653599793238
+void main() {
+  vUv = uv 
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0)
+}`;
