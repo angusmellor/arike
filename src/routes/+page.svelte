ee4d1f389902abd35a8ef6eaab27c389e3a28fb1
angusmellor <theangusmellor@gmail.com>
2023-10-30 13:33:18 +0000

    spotify embed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,35 @@
 <script>
-	import { Canvas } from "@threlte/core";
-	import Scene from "./Scene.svelte";
+	import Canvas from './Cavnas.svelte';
+	window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  const element = document.getElementById('embed-iframe');
+	const callback = (EmbedController) => {};
+  const options = {
+      uri: 'spotify:track:0lqqBVjUNnSeUdx3TmSCma',
+			width: "60%"
+    };
+  IFrameAPI.createController(element, options, callback);
+};
 </script>
-
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
+	<script src="https://open.spotify.com/embed/iframe-api/v1" async></script>
 </svelte:head>
 
-<Canvas>
-	<Scene />
-</Canvas>
+<Canvas />
+
+<h1>Padlock Arike</h1>
+
+<div class="spotify" id="embed-iframe"></div>
+
+
 
 <style>
-
+	h1 {
+		position: absolute;
+		margin-left: 50px;
+	}
 </style>
+
 
---
+++ src/routes/Cavnas.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Canvas } from "@threlte/core";
+	import Scene from "./Scene.svelte";
+</script>
+
+<div class="chrome">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  .chrome {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    z-index: -10;
+  }
+</style>
